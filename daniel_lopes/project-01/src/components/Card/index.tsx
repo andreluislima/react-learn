1bc199c4e86d03624753464e8e0ae7c5ddc28a13
andreluislima <andrelimag89@gmail.com>
2026-02-27 12:56:48 -0300

    feat: cria usestate

--- daniel_lopes/project-01/src/components/Card/index.tsx
+++ daniel_lopes/project-01/src/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 // PROPS => Propriedades que um componente pode receber
@@ -5,25 +6,30 @@ import "./styles.css";
 // INTERPOLAÇÃO: Colocar um valor de uma constante ou variavel dentro do JSX.
 // INTERPOLAÇÃO: <h1> {nomeVariavel} </h1>
 
-interface CardProps{
-    imgUrl: string;
-    name: string;
+interface CardProps {
+  imgUrl: string;
+  name: string;
 }
 
 export default function Card(props: CardProps) {
+  //   let follow = false;
+  const [follow, setFollow] = useState(false);
+
+  function handleFollowAndUnfollow() {
+    setFollow(!follow);
+  }
+
   return (
     <>
       <div className="card">
         <div>
-          <img
-            src={props.imgUrl}
-            alt="Carro - Onix 2020"
-          />
+          <img src={props.imgUrl} alt="Carro - Onix 2020" />
           <strong>{props.name}</strong>
         </div>
-        <button>Follow</button>
+        <button onClick={handleFollowAndUnfollow}>
+          {follow ? "Unfollow" : "Follow"}
+        </button>
       </div>
- 
     </>
   );
 }
