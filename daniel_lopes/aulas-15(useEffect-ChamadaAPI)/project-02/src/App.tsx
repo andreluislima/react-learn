277c0341c25be14b357228baa4f58efeb1f9ae95
andreluislima <andrelimag89@gmail.com>
2026-02-27 21:04:02 -0300

    aiça 16 - melhorando a integração.

--- daniel_lopes/aulas-15(useEffect-ChamadaAPI)/project-02/src/App.tsx
+++ daniel_lopes/aulas-15(useEffect-ChamadaAPI)/project-02/src/App.tsx
@@ -1,45 +1,74 @@
- 
- import { useEffect, useState } from 'react'
-import './App.css'
-import type { GithubUserType } from './types';
+import { useEffect, useState } from "react";
+import "./App.css";
+import type { GithubUserType } from "./types";
+import EmptyImage from "../src/assets/EmptyImage.png";
 
 function App() {
- 
-  const [user, setUser] = useState <GithubUserType | null> (null);
+  const [user, setUser] = useState<GithubUserType | null>(null);
+  const [input, setInput] = useState("");
 
-  useEffect(()=>{
-    (async () =>{
-      const response = await fetch(
-        "https://api.github.com/users/andreluislima21242"
-      )
-      if(response.status > 204){
-        alert("Falha ao encontrar o usuario")
-      }
+  async function searchUser() {
+    if (!input) {
+      setUser(null);
+      return;
+    }
+    const response = await fetch(
+      // "https://api.github.com/users/andreluislima"
+      `https://api.github.com/users/${input}`,
+    );
+    if (response.status > 204) {
+      console.warn("Falha ao encontrar o usuario");
+    }
 
-      const data: GithubUserType = (await response.json()) as GithubUserType;
-      setUser(data)
-      // console.log(await response.json());
+    const data: GithubUserType = (await response.json()) as GithubUserType;
+    setUser(data);
+    // console.log(await response.json());
+  }
+
+  useEffect(() => {
+    (async () => {
+      searchUser();
     })();
-  }, [])
+  }, []);
   return (
     <>
-       <div className='container'>
+      <div className="container">
         <div>
-          <strong>Nome: </strong>
-          <span>{user?.name}</span>
+          <input
+            type="text"
+            placeholder="Busque um usuário"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+
+          <button onClick={searchUser}>Buscar</button>
         </div>
 
-        <div>
-          <strong>Empresa: </strong>
-          <span>{user?.company}</span>
-        </div>
+        {user ? (
+          <div className="container-user">
+            <div>
+              <strong>Nome: </strong>
+              <span>{user?.name}</span>
+            </div>
 
-        <div>
-         <img src={user?.avatar_url} alt="imagem" />
-        </div>
-       </div>
+            <div>
+              <strong>Empresa: </strong>
+              <span>{user?.company}</span>
+            </div>
+
+            <div>
+              <img src={user?.avatar_url} alt="imagem" />
+            </div>
+          </div>
+        ) : (
+          <div className="container">
+            <img src={EmptyImage} alt="Empty" />
+            <span>Nenhum usuário encontrado.</span>
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
