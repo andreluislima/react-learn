0edbdb4708afec2c47a0eaf732b378269eb81f9f
andreluislima <andrelimag89@gmail.com>
2026-03-05 21:47:36 -0300

    feat: aula 27 - cria layout tela

--- daniel_lopes/modulo-2-projeto/1-app/src/App.tsx
+++ daniel_lopes/modulo-2-projeto/1-app/src/App.tsx
@@ -1,11 +1,41 @@
-import './App.css'
+import "./App.css";
 
 function App() {
- 
   return (
-    <h1>Teste</h1>
-  )
+    <div className="container-app">
+      <div className="container-app__header">
+        <div className="container-app__mask" />
 
+        <h1>TAREFAS</h1>
+        <input type="text" placeholder="Criar nova tarefa" />
+      </div>
+      <div className="container_tasks">
+        <ul className="content-tasks">
+          <div>
+            <li className="task-item">
+              {" "}
+              <input type="checkbox" name="" id="" />
+              Tarefa 1
+            </li>
+          </div>
+
+          <li className="content-tasks__actions">
+            <div>
+              <a href="">5 itens restantes</a>
+            </div>
+            <div>
+              <a href="">Todas</a>
+              <a href="">Ativas</a>
+              <a href="">Completadas</a>
+            </div>
+            <div>
+              <a href="">Limpar Completadas</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
