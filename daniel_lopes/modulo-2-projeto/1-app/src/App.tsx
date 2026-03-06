954829b4ae99ba9d190141c8500eabe2661ecc90
andreluislima <andrelimag89@gmail.com>
2026-03-05 23:03:39 -0300

    feat: cria novas tarefas

--- daniel_lopes/modulo-2-projeto/1-app/src/App.tsx
+++ daniel_lopes/modulo-2-projeto/1-app/src/App.tsx
@@ -1,22 +1,58 @@
+import { useState } from "react";
 import "./App.css";
+import type { TaskType } from "./types/TaskTypes";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState<TaskType[]>([
+
+    {
+      id: "1",
+      title: "Estudar React",
+      done: true,
+    },
+
+    {
+      id: "2",
+      title: "Estudar Java",
+      done: false,
+    },
+  ]);
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>){
+    if(input.length && event.key === "Enter"){
+      setTasks([...tasks, {id:"5", done:false, title:input}])
+      setInput("")
+     }
+  }
+
   return (
     <div className="container-app">
+
       <div className="container-app__header">
         <div className="container-app__mask" />
 
         <h1>TAREFAS</h1>
-        <input type="text" placeholder="Criar nova tarefa" />
+        <input type="text" placeholder="Criar nova tarefa"
+          value={input}
+          onChange={(event)=> setInput(event.target.value)}
+          onKeyDown={handleKeyDown}
+        />
       </div>
+
+
       <div className="container_tasks">
         <ul className="content-tasks">
           <div>
-            <li className="task-item">
-              {" "}
-              <input type="checkbox" name="" id="" />
-              Tarefa 1
-            </li>
+            {tasks.map((t) => (
+              <li
+                className={`task-item ${t.done ? "task-item__done" : ""}`}
+                key={t.id}
+              >
+                <input type="checkbox" checked={t.done} />
+                {t.title}
+              </li>
+            ))}
           </div>
 
           <li className="content-tasks__actions">
