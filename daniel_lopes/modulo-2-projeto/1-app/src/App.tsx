41ab4c917907f3aa6e50a8ea034905b2493e12ec
andreluislima <andrelimag89@gmail.com>
2026-03-05 23:32:44 -0300

    feat: cria marca e desmarca e qtd tarefas

--- daniel_lopes/modulo-2-projeto/1-app/src/App.tsx
+++ daniel_lopes/modulo-2-projeto/1-app/src/App.tsx
@@ -1,35 +1,44 @@
 import { useState } from "react";
 import "./App.css";
 import type { TaskType } from "./types/TaskTypes";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { useLocalStorage } from "usehooks-ts";
-
+import EmptyImage from "./assets/empty.svg";
 
 function App() {
   const [input, setInput] = useState("");
-  const [tasks, setTasks] = useLocalStorage<TaskType[]>("tasks-list",[]);
+  const [tasks, setTasks] = useLocalStorage<TaskType[]>("tasks-list", []);
+  const pendingTasksQtd = tasks.filter((tasks) => !tasks.done).length;
 
-  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>){
-    if(input.length && event.key === "Enter"){
-      setTasks([...tasks, {id:uuidv4(), done:false, title:input}])
-      setInput("")
-     }
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (input.length && event.key === "Enter") {
+      setTasks([...tasks, { id: uuidv4(), done: false, title: input }]);
+      setInput("");
+    }
+  }
+
+  function handleTaskToggle(id: string) {
+    setTasks((prevState) =>
+      prevState.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task,
+      ),
+    );
   }
 
   return (
     <div className="container-app">
-
       <div className="container-app__header">
         <div className="container-app__mask" />
 
         <h1>TAREFAS</h1>
-        <input type="text" placeholder="Criar nova tarefa"
+        <input
+          type="text"
+          placeholder="Criar nova tarefa"
           value={input}
-          onChange={(event)=> setInput(event.target.value)}
+          onChange={(event) => setInput(event.target.value)}
           onKeyDown={handleKeyDown}
         />
       </div>
-
 
       <div className="container_tasks">
         <ul className="content-tasks">
@@ -39,15 +48,26 @@ function App() {
                 className={`task-item ${t.done ? "task-item__done" : ""}`}
                 key={t.id}
               >
-                <input type="checkbox" checked={t.done} />
+                <input
+                  type="checkbox"
+                  checked={t.done}
+                  onChange={() => handleTaskToggle(t.id)}
+                />
                 {t.title}
               </li>
             ))}
+
+            {!tasks.length && (
+              <div className="container-empty">
+                <img src={EmptyImage} alt="empty" />
+                <h3>Nenhuma tarefa cadastrada!</h3>
+              </div>
+            )}
           </div>
 
           <li className="content-tasks__actions">
             <div>
-              <a href="">5 itens restantes</a>
+              <a href="">{pendingTasksQtd} itens restantes</a>
             </div>
             <div>
               <a href="">Todas</a>
